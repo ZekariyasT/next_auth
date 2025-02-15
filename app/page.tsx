e2fd2d7bc77ce810e3f7b11de3a1f77d197ccb97
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-15 18:17:10 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,16 @@
 "use server";
 import { auth } from "@/auth";
 import { SignInButton } from "./components/sign-in-button";
-import Image from "next/image";
+import Link from "next/link";
+import { SignOutButton } from "./components/sign-out-button";
 
 export default async function Home() {
   const session = await auth();
   if (session?.user) {
     return (
       <div>
-        <h1> NextAuth v5 + Next 15</h1>
-        <p>User signed in with name:{session.user.name}</p>
-        <p>User signed in with email: {session.user.email}</p>
-        {session.user.image && (
-          <Image
-            src={session.user.image}
-            width={48}
-            height={48}
-            alt={session.user.name ?? "Avator"}
-            style={{ borderRadius: "50%" }}
-          />
-        )}
+        <Link href="/user-info">User Info </Link>
+        <SignOutButton />
       </div>
     );
   }
