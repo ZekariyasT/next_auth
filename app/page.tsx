1be04a97b1c9c9b110a0c5b4dbc4be0fcf9b1a68
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-15 17:06:13 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 "use server";
 import { auth } from "@/auth";
 import { SignInButton } from "./components/sign-in-button";
+import Image from "next/image";
 
 export default async function Home() {
   const session = await auth();
-  console.log(session);
+  if (session?.user) {
+    return (
+      <div>
+        <h1> NextAuth v5 + Next 15</h1>
+        <p>User signed in with name:{session.user.name}</p>
+        <p>User signed in with email: {session.user.email}</p>
+        {session.user.image && (
+          <Image
+            src={session.user.image}
+            width={48}
+            height={48}
+            alt={session.user.name ?? "Avator"}
+            style={{ borderRadius: "50%" }}
+          />
+        )}
+      </div>
+    );
+  }
   return (
     <div>
       <p>You Are not Signed In</p>
